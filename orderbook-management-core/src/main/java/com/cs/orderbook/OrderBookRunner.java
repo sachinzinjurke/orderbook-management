59eb90b96fb2aa9c30ccb00ef831da25b04bceaa
Sachin Zinjurke <sachinzinjurke@gmail.com>
2019-05-21 22:29:10 +0530

    improving logger

--- orderbook-management-core/src/main/java/com/cs/orderbook/OrderBookRunner.java
+++ orderbook-management-core/src/main/java/com/cs/orderbook/OrderBookRunner.java
@@ -27,31 +27,31 @@ public class OrderBookRunner {
     	logger.info("*****************Staring application*************************");
         ApplicationContext ctx = new ClassPathXmlApplicationContext("orderbook-core-context.xml");
         OrderBookManager manager = (OrderBookManager)ctx.getBean("orderBookManager");
-        logger.info("Creating OrderBook for instrument : ",1);
+        logger.info("Creating OrderBook for instrument : {}",1);
         manager.createOrderBook(1);
-        logger.info("Opening OrderBook for instrument : ",1);
+        logger.info("Opening OrderBook for instrument : {}",1);
         manager.changeOrderBookStatus(1, OrderBookStatusType.OPEN);
         
-        logger.info("Adding orders to orderbook OrderBook for instrument : ",1);
+        logger.info("Adding orders to orderbook OrderBook for instrument : {}",1);
         manager.addOrderToOrderBook(1, new LimitOrder(1, 10,new Date(), 1, 20.10));
         manager.addOrderToOrderBook(1, new LimitOrder(2, 20,new Date(), 1, 30.10));
         manager.addOrderToOrderBook(1, new LimitOrder(2, 20,new Date(), 1, 5.10));
         manager.addOrderToOrderBook(1, new MarketOrder(3, 5,new Date(), 1, 10.10));
         manager.addOrderToOrderBook(1, new MarketOrder(4, 25,new Date(), 1, 15.10));
-        logger.info("Finished adding orders to orderbook OrderBook for instrument : ",1);
+        logger.info("Finished adding orders to orderbook OrderBook for instrument : {}",1);
         
         logger.info("Fetcing OrderBook Stats for instrument : ",1);
         OrderBookSummaryStats orderBookStats = manager.getOrderBookStats(1);
-        logger.info("Fetcing OrderBook Validity Stats for instrument : ",1);
+        logger.info("Fetcing OrderBook Validity Stats for instrument : {}",1);
         OrderBookValidityStats orderBookValidityStats = manager.getOrderBookValidityStats(1);
-        logger.info("Markign OrderBook as CLOSED for instrument : ",1);
+        logger.info("Markign OrderBook as CLOSED for instrument : {}",1);
         manager.changeOrderBookStatus(1, OrderBookStatusType.CLOSE);
         Execution execution=new Execution();
         execution.setPrice(15.00);
         execution.setQuantity(10);
-        logger.info("Executing OrderBook for instrument : ",1);
+        logger.info("Executing OrderBook for instrument : {}",1);
         manager.executeOrderBook(1,execution );
-        logger.info("Fetcing OrderBook Validity Stats after execution for instrument : ",1);
+        logger.info("Fetcing OrderBook Validity Stats after execution for instrument : {}",1);
         OrderBookValidityStats stats1 = manager.getOrderBookValidityStats(1);
         
         logger.info("*****************Exiting application*************************");
